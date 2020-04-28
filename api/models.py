cacc7d044c62f2095ec64d9a531f4857bdf91de4
Oleg Goncharov <oleg.gnch@gmail.com>
2020-04-29 00:09:12 +0300

    Substituting a custom User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    USER_ROLES = [
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Обычный пользователь'),
+    ]
+
+    confirmation_code = models.UUIDField(
+        blank=True,
+        editable=False,
+        null=True,
+        unique=True
+    )
+    role = models.CharField(max_length=20, choices=USER_ROLES, default='user')
+    bio = models.TextField()
