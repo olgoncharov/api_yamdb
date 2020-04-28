582f8d5671b48f0033f8bd2bdc79267e7f051847
ren.kaio <ren.kaio@yandex.ru>
2020-04-28 20:36:50 +0300

    csv_to_sqlite_importer.py script created & also added pandas lib to requirements.in

---
+++ csv_to_sqlite_importer.py
@@ -0,0 +1,32 @@
+import sqlite3
+import pandas
+
+
+def main():
+
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
+
+    tables = ["User", "Genre", "Category", "Title", "Review", "Comment"]
+    csv_files = ["users.csv", "genre.csv", "category.csv", "genre_title.csv", "review.csv", "comments.csv"]
+    index = 0
+
+    for filename in csv_files:
+        filepath = f'data/{filename}'
+        with open(filepath) as f:
+            f = pandas.read_csv(filepath)
+            f.to_sql(tables[index], conn, if_exists='append', index=False)
+            index += 1
+
+    # Проверка
+    for table in tables:
+        cursor.execute(f"SELECT * FROM {table} LIMIT 36")
+        table_content = cursor.fetchall()
+        print(table)
+        print(table_content)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
