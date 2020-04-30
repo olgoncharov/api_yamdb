e1066b2bc6ea23b6116289061014ead80b37ef30
Oleg Goncharov <oleg.gnch@gmail.com>
2020-05-01 01:13:16 +0300

    Settings email

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -131,3 +131,6 @@ REST_FRAMEWORK = {
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
