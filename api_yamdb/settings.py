332f93db8a9f7e90ce7ed9fca8945f99020c73f7
Oleg Goncharov <oleg.gnch@gmail.com>
2020-08-20 21:40:45 +0300

    Change database engine

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
